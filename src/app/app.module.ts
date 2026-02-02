3a8f374bcabb1d79f46ee5f5d48fbf3580fb44c0
amira-youssef <youssefamira964@gmail.com>
2026-02-02 14:47:40 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductComponent } from './product/product.component';
+import { ListSuggestionComponent } from './list-suggestion/list-suggestion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductComponent,
+    ListSuggestionComponent,
+
   ],
   imports: [
     BrowserModule,
